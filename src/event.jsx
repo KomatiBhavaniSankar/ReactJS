d7fc1f32cc0e7877b8972c37867ac99415eb8a37
KomatiBhavaniSankar <bhavanisankarkomati@gmail.com>
2025-07-15 15:35:57 +0530

    keyboard events

--- src/event.jsx
+++ src/event.jsx
@@ -1,19 +1,55 @@
 import React from "react";
+export function KeyboardEvent(){
+const handleKeyPress = (event) => {
+if(event.key == 'Enter'){
+    alert('Enter key pressed');
 
-function MouseOutEvent(){
+}
+};
 
 
-    const handleMouseLeave = () => alert('Mouse left');
+
+return <input type="text" onKeyPress={handleKeyPress} placeholder="press enter" />;
+}
+
+//Ketboard events
+
+export function KeyDownEvent() {
+
+    const handleKeyDown = (event) => {
+        alert(`key down:${event.key}`);
+
+    };
+   
+return <input type="text" onKeyDown={handleKeyDown} placeholder="press any key" />;
+}
+
+
+
+
+
+
+
+
+
+//     const handleMouseLeave = () => alert('Mouse left');
+
+// return (
+//     <div>
+
+//         <button onMouseLeave={handleMouseLeave} color="blue" >  Hover out me</button>
+//     </div>
+
+// );
+
+
 
 return (
-    <div>
 
-        <button onMouseLeave={handleMouseLeave} color="blue" >  Hover out me</button>
-    </div>
+<div>
+</div>
 
 );
 
 
-}
 
-export default MouseOutEvent;
